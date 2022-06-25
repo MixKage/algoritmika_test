d431f53053c39395ea1b2bbbc00271a1bc755115
maxnotscript <maxks.email@icloud.com>
2022-06-25 22:06:56 +0300

    Update test_things.py

--- Maxim's workspace/test_things.py
+++ Maxim's workspace/test_things.py
@@ -1,11 +1,1 @@
-import queue
-
-
-weapons = ["govno", "fignya", "andrey", "sraka", "huy", 'chlen']
-
-question = input("какой аружие? ").lower()
-
-if question in weapons[0:3]:
-    print("sosi chlen")
-elif question in weapons[3:6]:
-    print("poshel nahuy")
+print("асуждаю")
