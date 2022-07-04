f7dedeae6eb8b5ce42a6990e7f088224f17ff55e
maxnotscript <maxks.email@icloud.com>
2022-07-04 10:27:36 +0300

    fix: sound_play

--- Maxim's workspace/sound_play.py
+++ Maxim's workspace/sound_play.py
@@ -1,4 +1,8 @@
+from multiprocessing.connection import wait
 from playsound import playsound
+import asyncio
 
-def first_music():
+
+async def first_music():
+    await asyncio.wait()
     playsound("C:\\Users\\opmax\\Desktop\\algoritmika_test\\algoritmika_test\\Maxim's workspace\\Sounds\\Milky_Ways.mp3")
