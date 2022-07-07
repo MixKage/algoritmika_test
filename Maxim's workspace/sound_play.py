605fb9b5ff88fbfdf24fe22d035a94f04875bf78
maxnotscript <maxks.email@icloud.com>
2022-07-07 15:54:24 +0300

    add: sounds

--- Maxim's workspace/sound_play.py
+++ Maxim's workspace/sound_play.py
@@ -1,5 +1,9 @@
 from playsound import playsound
 
 
-def first_music():
-    playsound("C:\\Users\\opmax\\Desktop\\algoritmika_test\\algoritmika_test\\Maxim's workspace\\Sounds\\Milky_Ways.mp3", False)
+def first_music(bool):
+    playsound("C:\\Users\\opmax\\Desktop\\algoritmika_test\\algoritmika_test\\Maxim's workspace\\Sounds\\Milky_Ways.mp3", bool)
+def failure_sound(bool):
+    playsound("C:\\Users\\opmax\\Desktop\\algoritmika_test\\algoritmika_test\\Maxim's workspace\\Sounds\\failure.wav", bool)
+def success_sound(bool):
+    playsound("C:\\Users\\opmax\\Desktop\\algoritmika_test\\algoritmika_test\\Maxim's workspace\\Sounds\\success.wav", bool)
