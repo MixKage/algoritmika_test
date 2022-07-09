345f5a61e16dea91d0555cde385d26334a043cbe
maxnotscript <maxks.email@icloud.com>
2022-07-09 19:22:00 +0300

    move: all to ISEKAI folder

---
+++ Maxim's workspace/ISEKAI/sound_play.py
@@ -0,0 +1,9 @@
+from playsound import playsound
+
+
+def first_music(bool):
+    playsound("Maxim's workspace\\ISEKAI\\Sounds\\Milky_Ways.mp3", bool)
+def failure_sound(bool):
+    playsound("Maxim's workspace\\ISEKAI\\Sounds\\failure.wav", bool)
+def success_sound(bool):
+    playsound("Maxim's workspace\\ISEKAI\\Sounds\\success.wav", bool)
--- Maxim's workspace/sound_play.py
+++
@@ -1,9 +0,0 @@
-from playsound import playsound
-
-
-def first_music(bool):
-    playsound("Maxim's workspace\\Sounds\\Milky_Ways.mp3", bool)
-def failure_sound(bool):
-    playsound("Maxim's workspace\\Sounds\\failure.wav", bool)
-def success_sound(bool):
-    playsound("Maxim's workspace\\Sounds\\success.wav", bool)
