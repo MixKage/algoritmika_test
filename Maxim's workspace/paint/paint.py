0d6338d5c9aa4d25040910212efde89294cb60cc
MixKage <shishov265@gmail.com>
2022-07-14 17:33:09 +0300

    fix: paint.py

--- Maxim's workspace/paint/paint.py
+++ Maxim's workspace/paint/paint.py
@@ -1,35 +1,19 @@
-#   -- IMPORTS --
-import pygame as pg
-import sys
-import cfg
+import pygame
+BLUE = (0 , 0 , 255)
+screen = pygame.display.set_mode( (500, 500) )
+isPressed = False
 
-pg.init()
-
-sc = pg.display.set_mode((1000, 900))
-sc.fill(cfg.WHITE)
-pg.display.update()
- 
-pg.mouse.set_visible(False)  
- 
-clock = pg.time.Clock()   # FPS 
-
-while True:  # Главный цикл
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
-        if event.type == pg.MOUSEBUTTONDOWN:
-            pos = pg.mouse.get_pos()
-            pg.draw.rect(sc, cfg.BLACK, (pos[0] - 10, pos[1] - 10, 20, 20))
- 
-    sc.fill(cfg.WHITE)
- 
-    if pg.mouse.get_focused():
-        pos = pg.mouse.get_pos()
-        pg.draw.rect(
-            sc, cfg.BLUE, (pos[0] - 2.5,
-                           pos[1] - 2.5,
-                           5, 5))
+def drawCircle( screen, x, y ):
+  pygame.draw.circle( screen, BLUE, ( x, y ), 5 )
 
 
-    pg.display.update()
-    clock.tick(cfg.FPS)
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            isPressed = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            isPressed = False
+        elif event.type == pygame.MOUSEMOTION and isPressed == True:         
+            ( x, y ) = pygame.mouse.get_pos()       # returns the position of mouse cursor
+            drawCircle( screen, x, y )
+    pygame.display.flip()
