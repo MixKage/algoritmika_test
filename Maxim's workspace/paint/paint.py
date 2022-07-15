66ae4fea32863a5e7018ab0958afd15c6fc4c75a
maxnotscript <maxks.email@icloud.com>
2022-07-15 14:04:34 +0300

    fix: paint

--- Maxim's workspace/paint/paint.py
+++ Maxim's workspace/paint/paint.py
@@ -17,8 +17,15 @@
 import pygame
 import cfg
 
+pygame.font.init()
+
+font = pygame.font.SysFont(None, 12)
+
 def drawCircle( screen, x,y, color, size):
   pygame.draw.circle( screen, color, ( x,y ), size )
 
+def drawAline(screen, color,x,y):
+    pygame.draw.aalines(screen, color, False, (x,y))
+
 def showText(text, smooth, color):
-    text = cfg.font.render(text, smooth, color)
+    text = font.render(text, smooth, color)
