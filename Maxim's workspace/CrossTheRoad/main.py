c34ff9365403884edf36cd0dbc0012224b4362c3
maxnotscript <maxks.email@icloud.com>
2022-07-16 15:54:04 +0300

    fix: main

--- Maxim's workspace/CrossTheRoad/main.py
+++ Maxim's workspace/CrossTheRoad/main.py
@@ -19,15 +19,19 @@ import cfg
 turtle.colormode(255)
 
 
-player = turtle.Turtle(visible=False, shape="square")
-player.speed(100)
+player = turtle.Turtle(shape="square")
+player.hideturtle(); player.speed(0)
 player.color(cfg.colors[1]); player.fillcolor(cfg.colors[1])
 player.penup(); player.goto(0, -cfg.height//2); player.pendown()
 
-t1 = turtle.Turtle(visible=False, shape="turtle")
+text = turtle.Turtle()
+text.speed(0); text.hideturtle()
+
+cfg.text(text, "Hello! It's my new game.\n     Cross The Road", "red", "center", "Arial", 36, "bold")
+
+t1 = turtle.Turtle(shape="turtle")
+t1.hideturtle()
 t1.color(cfg.randomСolor()); t1.fillcolor(cfg.randomСolor())
-
-
 
 
 
