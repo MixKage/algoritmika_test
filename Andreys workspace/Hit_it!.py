c155b96b3fddf4c4594a1c410b2686afe4364816
CoolDev <andreymi2000@yandex.ru>
2022-07-21 17:15:18 +0300

    fix: Hit it!

--- Andreys workspace/Hit_it!.py
+++ Andreys workspace/Hit_it!.py
@@ -1,12 +1,19 @@
 from turtle import *
 from time import *
 
+score = 0
+
+dist1 = 0
+dist2 = 0
+dist3 = 0
+
 class Sprite(Turtle):
     def start(self, shapex, colorx, x_point, y_point):
         self.step = 10
         self.shape(shapex)
         self.color(colorx)
         self.goto(x_point, y_point)
+        self.speed(0)
 
     def set_move(self):
         if self.xcor() >= 100:
@@ -48,6 +55,10 @@ t4.left(90)
 t4.penup()
 t4.start('triangle', 'green', 0, 75)
 
+t5 = Sprite()
+t5.penup()
+t5.start('circle', 'red', 100, 100)
+
 scr = Screen()
 scr.listen()
 t1.getscreen()
@@ -58,8 +69,32 @@ scr.onkey(t1.move_right, 'd')
 scr.onkey(t1.move_left, 'a')
 
 while True:
+    dist1 = t1.distance(t2.xcor(), t2.ycor())
+    dist2 = t1.distance(t3.xcor(), t3.ycor())
+    dist3 = t1.distance(t4.xcor(), t4.ycor())
+    if dist1 <= 20 or dist2 <= 20:
+        t1.hideturtle()
+        break
+    elif dist3 <= 5:
+        t1.goto(0, -75)
+        score += 1
+        sleep(0.1)
+    if score == 0:
+        t5.color('red')
+    elif score == 1:
+        t5.color('yellow')
+    elif score == 2:
+        t5.color('orange')
+    elif score >= 3:
+        t5.color('green')
+        t2.hideturtle()
+        t3.hideturtle()
+        break
+    
     t2.set_move()
     t2.make_step()
     t3.set_move()
     t3.make_step()
     sleep(0.1)
+
+input('yay')
