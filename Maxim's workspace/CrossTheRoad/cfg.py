1bd7aa3de1029ef8af0e6c9d4dc494fbae62d4e4
maxnotscript <maxks.email@icloud.com>
2022-07-30 22:01:22 +0300

    fix: CrossTheRoad

--- Maxim's workspace/CrossTheRoad/cfg.py
+++ Maxim's workspace/CrossTheRoad/cfg.py
@@ -15,41 +15,48 @@ import turtle
 import numpy as np
 
 
-#  -------- Colors --------
-colors = [
-    "black",
-    "red",
-    "lightred",
-    "green",
-    "blue",
-    "yellow",
-    "cyan",
-    "magenta",
-    "purple",
-    "gray",
-    "white"
-]
-
-
 #  -------- Variables --------
-weight = 400
-height = 400
-x = -500
-y = -500
+weight = 200
+height = 150
+
+start_pos = (0, -height//2)
+
+total_score = 0
+
 
 #  -------- All Functions --------
 def randomСolor(): #  Random Rgb Color
     return list(np.random.choice(range(256), size=3))
 
-def text(name, text, color, align, font, size, type):  #  Show text by Turtle
-    name.color(color)
-    name.write(text,align=align,font=(font, size, type))
-
 
 #  -------- Classes --------
-class createTurtle:
-    def __init__(self, color, shape, name, fillcolor):
-        name = turtle.Turtle(visible=False)
-        name.shape(shape)
-        name.color(color)
-        name.fillcolor(fillcolor)
+class Sprite(turtle.Turtle):
+    def __init__(self, x,y, shape="square", color=randomСolor(), step=10):
+        super().__init__()
+        self.speed(0)
+        self.penup()
+        self.goto(x,y)
+        self.shape(shape)
+        self.color(color)
+        self.step = step
+
+    def text(self, text, color="Black", align="Center", font="Arial", size=32, type="normal"):  #  Show text by Turtle
+        self.color(color)
+        self.write(text,align=align,font=(font, size, type))
+    
+    def move_up(self):
+        self.goto(self.xcor(), self.ycor() + self.step)
+
+    def move_down(self):
+        self.goto(self.xcor(), self.ycor() - self.step)
+
+    def move_left(self):
+        self.goto(self.xcor() - self.step, self.ycor())
+
+    def move_right(self):
+        self.goto(self.xcor() + self.step, self.ycor())
+
+    def is_collide(self,object):
+        return self.distance(object.xcor(), object.ycor()) < 30
+
+    
--- Maxim's workspace/CrossTheRoad/main.py
+++ Maxim's workspace/CrossTheRoad/main.py
@@ -15,43 +15,33 @@ import turtle
 import cfg
 
 
+
 #  -------- Turtles --------
 turtle.colormode(255)
 
-line = turtle.Turtle()
-line.speed(0); line.hideturtle(); line.pensize(1)
-line.penup(); line.goto(cfg.x,cfg.y); line.pendown()
-for i in range(50):
-    line.forward(1000)
-    cfg.y += 20
-    line.penup(); line.goto(cfg.x,cfg.y); line.pendown()
 
-player = turtle.Turtle(shape="square")
-player.speed(0); # player.hideturtle()
-player.color(cfg.colors[1]); player.fillcolor(cfg.colors[1])
-player.penup(); player.goto(0, -cfg.height+50); player.pendown()
-player.shapesize(1,1,5)
-
-text = turtle.Turtle()
-text.speed(0); text.hideturtle()
-
-goal = turtle.Turtle(shape="square")
-goal.color(cfg.colors[0]); goal.fillcolor(cfg.randomСolor())
-goal.speed(0); # goal.hideturtle()
-goal.penup(); goal.goto(0, cfg.height-50); goal.pendown()
-goal.shapesize(2,2,1)
 
 
 
 # cfg.text(text, "Hello! It's my new game.\n     Cross The Road", "red", "center", "Arial", 36, "bold")
 
+player = cfg.Sprite(0,-cfg.height//2, "circle", "orange")
+enemy1 = cfg.Sprite(-cfg.weight, 0, 'square', cfg.randomСolor(), 15)
+enemy2 = cfg.Sprite(cfg.weight, 70, 'square', cfg.randomСolor(), 15)
+goal = cfg.Sprite(0, cfg.height, 'triangle', 'green', 20)
+ 
+scr = player.getscreen()
+scr.listen()
+scr.onkey(player.move_up, 'Up')
+scr.onkey(player.move_left, 'Left')
+scr.onkey(player.move_right, 'Right')
+scr.onkey(player.move_down, 'Down')
 
 
-t1 = turtle.Turtle(shape="turtle")
-t1.hideturtle()
-t1.color(cfg.randomСolor()); t1.fillcolor(cfg.randomСolor())
 
-
+while cfg.total_score < 3:
+    if player.is_collide(goal): player.goto(cfg.start_pos); cfg.total_score += 1
+    elif player.is_collide(enemy1) or player.is_collide(enemy2): goal.hideturtle(); break
 
 #  -------- Other --------
 turtle.exitonclick()
